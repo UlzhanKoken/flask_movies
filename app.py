4f5079af366fc96bd10afecc0be989a20e1bddd4
Ulzhan <kokenova.ulzhan@gmail.com>
2020-05-23 19:56:29 +0600

    Add hell

--- app.py
+++ app.py
@@ -31,10 +31,51 @@ def movie():
 
 
 # @app.route('/cinema/<int:cinema_id>')
-@app.route('/cinema/<cinema_id>')
+@app.route('/hello/<cinema_id>')
 def cinema(cinema_id):
     with open('theatres.json') as f:
         theatres = json.load(f)
         name = theatres[cinema_id]
     # return 'cinema %d' % cinema_id
     return 'cinema ' + name
+
+# extended theatre above
+@app.route('/hell')
+def hell():
+    with open('theatres.json') as f:
+        theatres = json.load(f)
+        theatre_to_id = {}
+    for theatre_id, name in theatres.items():
+        theatre_to_id[name] = theatre_id
+    with open('db.json') as f:
+        movies = json.load(f)
+    schedule = {}
+    for movie in movies:
+        theatre = movie['theatre']
+        if theatre not in schedule:
+            schedule[theatre] = {
+                "movies": [],
+                "id": theatre_to_id[theatre]
+            }
+        schedule[theatre]['movies'].append(
+            {
+                'name': movie['name'],
+                'time': movie['time']
+            }
+        )
+    return render_template("hell.html", schedule=schedule)
+
+
+@app.route('/cinema/<cinema_id>')
+def cinemas(cinema_id):
+    with open('db.json') as f:
+        movies = json.load(f)
+    with open('theatres.json') as f:
+        theatres = json.load(f)
+        theatre = theatres[cinema_id]
+        result = []
+        # for movie in movies:
+        #     if movie['theatre'] == theatre:
+        #         result.append(movie)
+        result = [movie for movie in movies if movie['theatre'] == theatre]
+    return render_template('cinema.html', theatre=theatre, movies=result)
